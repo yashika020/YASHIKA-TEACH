ddb56a7570a62229a3dd7d39b317ec7d7a73e8e5
yashika020 <luckykansal446@gmail.com>
2024-11-23 11:15:47 +0530

    Add files via upload

---
+++ firstjavaprogram.java
@@ -0,0 +1,14 @@
+
+class main {
+    public static void main(String[] args) {
+       int num  = 10;
+       if (num % 2 == 0){
+        System.out.println("even");
+       }else{
+        System.out.println("odd");
+       }
+        
+    }
+}  
+
+        
