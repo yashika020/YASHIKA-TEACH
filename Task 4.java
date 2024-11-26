352a47123acb91c94db842a26b3cf2d0938b8bc6
yashika020 <luckykansal446@gmail.com>
2024-11-26 22:40:55 +0530

    Add files via upload

---
+++ Task 4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+ class main {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int multiplier = 1 ;
+        while(multiplier<=10){
+            System.out.println(i * multiplier);
+        
+        multiplier++;
+        }
+        
+        
+        
+
+    }
+}
