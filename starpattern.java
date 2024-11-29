66042ad486f0888843aaa1a9f48057a6e29bc20a
yashika020 <luckykansal446@gmail.com>
2024-11-29 22:14:09 +0530

    Add files via upload

---
+++ starpattern.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class starpattern{
+    public static void main(String args[]){
+        System.out.println("enter the numeber of rows");
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int i ,j ;
+        for(i =1 ; i <=rows ; i++){
+            for(j=1 ; j<=i ; j++){
+        
+        System.out.print("*");
+        }
+        
+        System.out.println( );
+    }
+        
+    }
+   
+
+    }
